026944cb79def4cf494cdc3c552f57092d1aefe2
John Helfert <sharkanana@gmail.com>
2015-10-26 14:52:15 -0400

    Showing rows for each movement, and icon

--- ui/public/js/app.js
+++ ui/public/js/app.js
@@ -7,8 +7,12 @@
 
         var me = this;
 
+        $scope.isLoading = true;
+
         $http.get('/initialLoad').success(function(response) {
-            console.log(response);
+
+            $scope.movements = response;
+            $scope.isLoading = false;
         });
     });
 })();
