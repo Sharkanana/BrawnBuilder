c7de34873cc1bf8bd3a72c7e2b7dcad0b6e8b570
John Helfert <sharkanana@gmail.com>
2015-10-27 10:15:09 -0400

    Polishing form

--- ui/public/js/app.js
+++ ui/public/js/app.js
@@ -7,14 +7,29 @@
 
         $scope.isLoading = true;
 
-        $scope.dates = [];
-        $scope.weights = [];
-        $scope.reps = [];
+        $scope.dates = {};
+        $scope.weights = {};
+        $scope.reps = {};
 
         $http.get('/initialLoad').success(function(response) {
             $scope.movements = response;
+
+            for(var i = 0; i < response.length; i++) {
+                var movement = response[i];
+
+                $scope.dates[movement.name] = new Date();
+            }
+
             $scope.isLoading = false;
         });
+
+        $scope.addLog = function(movement) {
+            console.log(movement);
+        };
+
+        $scope.canAdd = function(movement) {
+            return $scope.dates[movement] && $scope.weights[movement] && $scope.reps[movement];
+        };
 
         $scope.calculateMax = function(movement) {
 
