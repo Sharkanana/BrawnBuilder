aef2ead2a323c6cc11fae05a0e40d65a98f758e3
John Helfert <sharkanana@gmail.com>
2015-10-27 10:05:53 -0400

    Add log forms

--- ui/public/js/app.js
+++ ui/public/js/app.js
@@ -7,9 +7,24 @@
 
         $scope.isLoading = true;
 
+        $scope.dates = [];
+        $scope.weights = [];
+        $scope.reps = [];
+
         $http.get('/initialLoad').success(function(response) {
             $scope.movements = response;
             $scope.isLoading = false;
         });
+
+        $scope.calculateMax = function(movement) {
+
+            var weight = $scope.weights[movement],
+                reps = $scope.reps[movement];
+
+            if(!weight || !reps)
+                return '';
+
+            return Math.round(weight * (1+(reps/30)));
+        };
     });
 })();
